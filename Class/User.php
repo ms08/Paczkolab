21985352abca7b22cb8913fcf7bec9c75de880ba
Michał Stanek <michal.stanek@me.com>
2017-02-06 15:47:39 +0100

    first commit1.5

--- Class/User.php
+++ Class/User.php
@@ -20,34 +20,16 @@ class User
     $this->hashedPassword='';
   }
 
-    /**
-     * Get the value of Id
-     *
-     * @return mixed
-     */
     public function getId()
     {
         return $this->id;
     }
 
-
-    /**
-     * Get the value of Adress
-     *
-     * @return mixed
-     */
     public function getAdress()
     {
         return $this->adress;
     }
 
-    /**
-     * Set the value of Adress
-     *
-     * @param mixed adress
-     *
-     * @return self
-     */
     public function setAdress($adress)
     {
         $this->adress = $adress;
@@ -55,23 +37,11 @@ class User
         return true;
     }
 
-    /**
-     * Get the value of Name
-     *
-     * @return mixed
-     */
     public function getName()
     {
         return $this->name;
     }
 
-    /**
-     * Set the value of Name
-     *
-     * @param mixed name
-     *
-     * @return self
-     */
     public function setName($name)
     {
         $this->name = $name;
@@ -79,23 +49,11 @@ class User
         return true;
     }
 
-    /**
-     * Get the value of Surname
-     *
-     * @return mixed
-     */
     public function getSurname()
     {
         return $this->surname;
     }
 
-    /**
-     * Set the value of Surname
-     *
-     * @param mixed surname
-     *
-     * @return self
-     */
     public function setSurname($surname)
     {
         $this->surname = $surname;
@@ -103,23 +61,11 @@ class User
         return true;
     }
 
-    /**
-     * Get the value of Credits
-     *
-     * @return mixed
-     */
     public function getCredits()
     {
         return $this->credits;
     }
 
-    /**
-     * Set the value of Credits
-     *
-     * @param mixed credits
-     *
-     * @return self
-     */
     public function setCredits($credits)
     {
         $this->credits = $credits;
@@ -127,26 +73,15 @@ class User
         return true;
     }
 
-    /**
-     * Get the value of Hashed Password
-     *
-     * @return mixed
-     */
     public function getHashedPassword()
     {
         return $this->hashedPassword;
     }
 
-    /**
-     * Set the value of Hashed Password
-     *
-     * @param mixed hashedPassword
-     *
-     * @return self
-     */
-    public function setHashedPassword($hashedPassword)
+    public function setHashedPassword($password)
     {
-        $this->hashedPassword = $hashedPassword;
+        $hashedPassword=password_hash($password, PASSWORD_BCRYPT);
+        $this->password = $hashedPassword;
 
         return true;
     }
@@ -157,6 +92,7 @@ $user1 = new User();
 var_dump($user1);
 $user2 = new User;
 $user2->setName('marcin');
+$user2->setHashedPassword('sddsfsdf');
 var_dump($user2);
 
  ?>
